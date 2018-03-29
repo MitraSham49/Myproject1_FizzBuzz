2aa19cdd489cf6b969c7b8a1024afa4d8b63060c
MitraSham49 <mairmeds@gmail.com>
2018-03-29 16:47:44 -0400

    Add few line for multiply 7,17,23

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for(int x = 1; x < 100; x = x + 1) {
+        int number;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        number = scan.nextInt();
+        for(int x = 1; x < number+1 ; x = x + 1) {
 
            /* System.out.print(x + " ");
             System.out.println();
@@ -25,8 +29,30 @@ public class Main {
             System.out.print(x + " ");
             System.out.println();
 
+
+            }
+        for(int x = 1; x < number+1 ; x = x + 1) {
+            if ((x % 7) == 0) {
+                System.out.print(x + " multiples of 7");
+                System.out.println();
+            }
         }
 
+        for(int x = 1; x < number+1 ; x = x + 1) {
+            if ((x % 17) == 0) {
+
+                System.out.print(x + " multiples of 17");
+                System.out.println();
+            }
+        }
+        for(int x = 1; x < number+1 ; x = x + 1) {
+            if ((x % 23) == 0) {
+
+                System.out.print(x + " multiples of 23 ");
+                System.out.println();
+            }
+
+        }
 
     }
 
